71695c95d3477cb0a2c681fb4ac1402e984c1298
Justin Zhang <jing.justin@gmail.com>
2021-01-20 17:33:57 -0500

    new thank you page added

--- client/src/pages/ThankYou/ThankYouData.js
+++ client/src/pages/ThankYou/ThankYouData.js
@@ -3,7 +3,7 @@ export const thankYouMessage= {
     lightTextDescription: false,
     headline: "Thank You for your submission",
     description: "We have sent you a copy of your message to your inbox. Check your email within the next 1-2 business days for our reply.",
-    imgStart: "",
-    img: "/images/KidsWithSlime.jpg",
-    alt: "Children crafting"
+    imgStart: "start",
+    img: "/images/KidsWithCollie.jpg",
+    alt: "Children With Dog"
 }
